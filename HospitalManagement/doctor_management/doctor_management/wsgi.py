44885319d7b0e4827feaae62e8e2799cab874288
Kien Do <ddki1912@gmail.com>
2024-05-30 12:36:49 +0700

    init project

--- HospitalManagement/doctor_management/doctor_management/wsgi.py
+++ HospitalManagement/doctor_management/doctor_management/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HospitalManagement project.
+WSGI config for doctor_management project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HospitalManagement.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doctor_management.settings')
 
 application = get_wsgi_application()
--- HospitalManagement/payment_management/payment_management/asgi.py
+++ HospitalManagement/payment_management/payment_management/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for HospitalManagement project.
+ASGI config for payment_management project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HospitalManagement.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'payment_management.settings')
 
 application = get_asgi_application()
