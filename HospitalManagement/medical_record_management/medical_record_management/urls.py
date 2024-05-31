80bd4e389ba32a0d93027bc145f10c5c427eef8f
HungND <hungnguyendoan.2002@gmail.com>
2024-05-31 19:35:22 +0700

    update

--- HospitalManagement/medical_record_management/medical_record_management/urls.py
+++ HospitalManagement/medical_record_management/medical_record_management/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from medical_record.views import MedicalRecordAPIView
+from medical_record.views import (
+    RecordCreateAPIView,
+    RecordDetailAPIView,
+    RecordUpdateAPIView,
+    RecordListByPatientOrDoctorAPIView,
+    RecordSearchAPIView,
+)
 
 urlpatterns = [
-    path('api/medical-records/', MedicalRecordAPIView.as_view(), name='medical-record-list'),
-    path('api/medical-records/<int:pk>/', MedicalRecordAPIView.as_view(), name='medical-record-detail'),
+    path('api/records/create', RecordCreateAPIView.as_view(), name='record-create'),
+    path('api/records/<int:id>', RecordDetailAPIView.as_view(), name='record-detail'),
+    path('api/records/update/<int:id>', RecordUpdateAPIView.as_view(), name='record-update'),
+    path('api/records', RecordListByPatientOrDoctorAPIView.as_view(), name='record-list-by-role'),
+    path('api/records/patients/<int:patient_id>/search', RecordSearchAPIView.as_view(), name='record-search'),
 ]
