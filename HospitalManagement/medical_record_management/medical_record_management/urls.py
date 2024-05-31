4f37b5dd761917824273d7e9db06135cf21d86ad
HungND <hungnguyendoan.2002@gmail.com>
2024-05-31 14:16:42 +0700

    init crud method

--- HospitalManagement/medical_record_management/medical_record_management/urls.py
+++ HospitalManagement/medical_record_management/medical_record_management/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from medical_record.views import MedicalRecordAPIView
 
 urlpatterns = [
-    # Other URL patterns...
-    path('api/medical-records/', MedicalRecordAPIView.as_view(), name='medical-record-api'),
+    path('api/medical-records/', MedicalRecordAPIView.as_view(), name='medical-record-list'),
+    path('api/medical-records/<int:pk>/', MedicalRecordAPIView.as_view(), name='medical-record-detail'),
 ]
