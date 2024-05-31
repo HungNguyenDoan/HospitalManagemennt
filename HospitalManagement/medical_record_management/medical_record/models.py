14d27ef3b2f6148b7cd18685967330d7c258e4b2
HungND <hungnguyendoan.2002@gmail.com>
2024-05-31 14:16:42 +0700

    init crud method

--- HospitalManagement/medical_record_management/medical_record/models.py
+++ HospitalManagement/medical_record_management/medical_record/models.py
@@ -1,20 +1,19 @@
-from djongo import models
+from django.db import models
 
 class MedicalTest(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        db_table = 'medical_tests'
+
 class MedicalRecord(models.Model):
-    _id = models.ObjectIdField()
     patient_id = models.CharField(max_length=255)
     doctor_id = models.CharField(max_length=255)
     date = models.DateField()
     reason = models.CharField(max_length=255)
     symptoms = models.CharField(max_length=255)
     first_diagnosis = models.CharField(max_length=255)
-    medical_tests = models.ArrayField(
-        model_container=MedicalTest,
-        default=list
-    )
+    medical_tests = models.ManyToManyField(MedicalTest)
     final_diagnosis = models.CharField(max_length=255)
     treatment_plan = models.CharField(max_length=255)
     note = models.TextField()
--- HospitalManagement/medical_record_management/medical_record/views.py
+++ HospitalManagement/medical_record_management/medical_record/views.py
@@ -1,39 +1,45 @@
-from django.shortcuts import get_object_or_404
+import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import MedicalRecord
 from .serializer import MedicalRecordSerializer
 
 class MedicalRecordAPIView(APIView):
-    """
-    A simple APIView for CRUD operations on MedicalRecord model.
-    """
 
-    def get(self, request):
-        queryset = MedicalRecord.objects.all()
-        serializer = MedicalRecordSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_patient_info(self, patient_id):
+        url = f'http://127.0.0.1:8000/api/patient/{patient_id}/'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+    def get(self, request, pk=None):
+        if pk is not None:
+            medical_record = get_object_or_404(MedicalRecord, pk=pk)
+            patient_info = self.get_patient_info(medical_record.patient_id)
+            serializer = MedicalRecordSerializer(medical_record)
+            data = serializer.data
+            if patient_info:
+                data['patient_info'] = patient_info
+            data.pop('patient_id', None)
+            return Response(data)
+        else:
+            queryset = MedicalRecord.objects.all()
+            serializer = MedicalRecordSerializer(queryset, many=True)
+            data_list = serializer.data
+            for data in data_list:
+                patient_info = self.get_patient_info(data['patient_id'])
+                if patient_info:
+                    data['patient_info'] = patient_info
+                data.pop('patient_id', None)
+            return Response(data_list)
 
     def post(self, request):
         serializer = MedicalRecordSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def get_object(self, pk):
-        return get_object_or_404(MedicalRecord, pk=pk)
-
-    def get(self, request, pk=None):
-        medical_record = self.get_object(pk)
-        serializer = MedicalRecordSerializer(medical_record)
-        return Response(serializer.data)
-
-    def put(self, request, pk=None):
-        medical_record = self.get_object(pk)
-        serializer = MedicalRecordSerializer(medical_record, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
